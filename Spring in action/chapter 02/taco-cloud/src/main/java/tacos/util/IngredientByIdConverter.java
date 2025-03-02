ff066cdff484b03658a679477423fb0a9d750565
enzzo <enzzomatrix@gmail.com>
2025-03-02 21:42:32 +1100

    upd

---
+++ Spring in action/chapter 02/taco-cloud/src/main/java/tacos/util/IngredientByIdConverter.java
@@ -0,0 +1,33 @@
+package tacos.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import tacos.model.Ingredient;
+import tacos.model.Ingredient.Type;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+	private Map<String, Ingredient> ingredientMap = new HashMap<>();
+
+	@Override
+	public Ingredient convert(String source) {
+		return ingredientMap.get(source);
+	}
+
+	public IngredientByIdConverter() {
+		this.ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+		this.ingredientMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+		this.ingredientMap.put("GRBF", new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+		this.ingredientMap.put("CARN", new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+		this.ingredientMap.put("TMTO", new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+		this.ingredientMap.put("LETC", new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+		this.ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
+		this.ingredientMap.put("JACK", new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+		this.ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
+		this.ingredientMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+	}
+}
