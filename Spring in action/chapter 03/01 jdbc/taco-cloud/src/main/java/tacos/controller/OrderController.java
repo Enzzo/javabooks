aebedd794d37e98810536f86b6a60ab903f1437a
enzzo <enzzomatrix@gmail.com>
2025-03-06 07:05:18 +1100

    upd

--- Spring in action/chapter 03/01 jdbc/taco-cloud/src/main/java/tacos/controller/OrderController.java
+++ Spring in action/chapter 03/01 jdbc/taco-cloud/src/main/java/tacos/controller/OrderController.java
@@ -1,5 +1,6 @@
 package tacos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +12,20 @@ import org.springframework.web.bind.support.SessionStatus;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import tacos.model.TacoOrder;
+import tacos.repository.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+	private OrderRepository orderRepo;
 	
+	@Autowired
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+
 	@GetMapping("/current")
 	public String orderForm() {
 		return "orderForm";
@@ -32,7 +40,7 @@ public class OrderController {
 			return "orderForm";
 		}
 		
-		log.info("Order {} submitted", order);
+		orderRepo.save(order);
 		sessionStatus.setComplete();
 		return "redirect:/";
 	}
