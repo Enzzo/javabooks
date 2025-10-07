06b9029936ea490f023581bad39e115851427cb0
enzzo <enzzomatrix@gmail.com>
2025-10-07 21:13:30 +1100

    upd

--- Spring in action/chapter 05/5.3.3 oauth/taco-cloud/src/main/java/tacos/configuration/SecurityConfig.java
+++ Spring in action/chapter 05/5.3.3 oauth/taco-cloud/src/main/java/tacos/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package tacos.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,7 +34,7 @@ public class SecurityConfig {
 				.passwordParameter("pwd")
 				.defaultSuccessUrl("/design", true))
 			.headers(headers -> headers
-					.frameOptions(frameOptions -> frameOptions.sameOrigin())
+					.frameOptions(FrameOptionsConfig::sameOrigin)
 			)
 			.build();
 	}
--- Spring in action/chapter 05/5.3.3 oauth/taco-cloud/src/main/java/tacos/repository/IngredientRepository.java
+++ Spring in action/chapter 05/5.3.3 oauth/taco-cloud/src/main/java/tacos/repository/IngredientRepository.java
@@ -1,8 +1,5 @@
 package tacos.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
